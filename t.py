207e1ec9c9e368fb114645fbbb242bab94088534
ammekourourou <65537601+ammekourourou@users.noreply.github.com>
2020-05-18 10:43:14 +0100

    C4extractor
    In this tool you can even hack facebook but (with link only) everything work with links here

---
+++ t.py
@@ -0,0 +1,58 @@
+aui = {'gy':'www.card generators.com','jio':'https://www.shrohat.online/2019/10/2020.html?m=1','ygf':'https://www.ip-tracker.org/resolve/'}
+drt = ("https://www.ip-tracker.org/resolve/")
+xs ="NO CHOICE"          
+print("  €-----<C4 extractor>-----¥")
+print("        by ammekourourou")
+print("[1]gift card generator")
+print("[2]fly facebook acoounts")
+print("[3]website ip resolver")
+print("[4]website secret info")
+print("[5]email hacker")
+print("[6]bank accounts hacker")
+print("[7]credit card builder")
+print("more coming soon")
+while True:
+   lied = input("[q=quit]:---->")
+   if lied =='q':
+            break
+   elif lied == '1':
+        for gyt in range(1,2):
+              print(gyt)
+              print("to generate gift cards you can go here")
+              print(aui.get('gy'))
+   elif lied == '2':       
+       for g in range(1,2):
+            print(g)
+            print("here you can fly facebokk accounts with code")
+            print(aui.get('jio'))
+   elif lied == '3':
+        for md in range(1,2):
+            print(md)
+            print("here you can resolve any website IP")
+            print(drt)
+   elif lied =='4': 
+        for h in range(1,2):    
+            print(h)    
+            print("here you can even see website admin info")
+            print("http://w3bin.com/")
+   elif lied =='5':
+         for b in range(1,2):                          
+            print(b)
+            print('here you can generate emails')
+            print("https://gmailnator.com/bulk-emails")
+   elif lied =='6':       
+        print("a board of bank accounts generators")
+        print("-- https://www.getcreditcardinfo.com/visabulkgenerator.php")
+        print("-- https://cardguru.io/bulk-credit-card-generator")
+        print("-- https://www.bestccgen.com/bulk-card-generator.php")
+        print("--(VERY POWERFUL)-- https://generator-credit-card.com/ ")
+        print("-- (MOST POWERFUL)-- https://maxnos.com/valid-credit-card-numbers-with-cvv-and-expiration-date-2019/")      
+        print("credit card validator: ")
+        print("-- https://www.freeformatter.com/credit-card-number-generator-validator.html")
+   elif lied =='7':
+        for x in range(1,2):
+            print(x)   
+            print("here you can build fake credit card")  
+            print("https://names.igopaygo.com/credit-card/builder")
+else:
+     print(xs)
